6f579adb468186f72857b81479f9cdd36c47e507
Joris Hageman <jorishageman04@gmail.com>
2023-10-10 12:39:55 +0200

    Feature(Inventory): Inventory screen added

--- NinjaProject/Models/Ninja.cs
+++ NinjaProject/Models/Ninja.cs
@@ -22,10 +22,5 @@ namespace NinjaApplication.Models
         public int Gold { get; set; }
 
         public List<InventoryItem>? Inventory { get; set; }
-
-        /*public int TotalStrength => Inventory?.Sum(item => item.Strength) ?? 0;
-        public int TotalIntelligence => Inventory?.Sum(item => item.Intelligence) ?? 0;
-        public int TotalAgility => Inventory?.Sum(item => item.Agility) ?? 0;
-        public int TotalGoldValue => Inventory?.Sum(item => item.GoldValue) ?? 0;*/
     }
 }
