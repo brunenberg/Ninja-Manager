7c7a40b87a2df5d16cb4da7e367ebcb18059b67d
Wouter <94843584+Delluci@users.noreply.github.com>
2023-10-19 13:19:47 +0200

    feat(SeedData): add seed data (#22)

--- NinjaProject/Migrations/20231019104646_SeedData.Designer.cs
+++ NinjaProject/Migrations/20231019104646_SeedData.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NinjaProject.Database;
 
@@ -10,9 +11,11 @@ using NinjaProject.Database;
 namespace NinjaProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231019104646_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NinjaProject/Migrations/20231019104646_SeedData.cs
@@ -0,0 +1,94 @@
+﻿using Humanizer;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.IdentityModel.Tokens;
+
+#nullable disable
+
+namespace NinjaProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryId",
+                table: "gear",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.InsertData(
+                table: "ninja",
+                columns: new[] { "name", "gold" },
+                values: new object[,]
+                {
+                    { "Wouter", "1000" },
+                    { "Joris", "1200" },
+                    { "Pieter-Jan Flatgebouw", "5000000" },
+                    { "Zwerver", "10" },
+                    { "Ninjaman123", "2000" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "gear_category",
+                column: "category",
+                values: new object[]
+                {
+                    "Head",
+                    "Necklace",
+                    "Chest",
+                    "Hands",
+                    "Ring",
+                    "Feet"
+                });
+
+            migrationBuilder.InsertData(
+                table: "gear",
+                columns: new[] { "name", "goldvalue", "strength", "intelligence", "agility", "CategoryId" },
+                values: new object[,]
+                {
+                    { "Diamanten Helm", 2000, 1000, 100, 300, "Head" },
+                    { "Gucci Pet", 300, -100, -400, 500, "Head" },
+                    { "Hoge Hoed", 1000, 0, 1000, 300, "Head" },
+
+                    { "Diamanten Ketting", 3000, 500, 700, 50, "Necklace" },
+                    { "Gouden Ketting", 500, 300, 400, 100, "Necklace" },
+                    { "Zilveren Ketting", 200, 100, 100, 150, "Necklace" },
+
+                    { "Plaatpantser", 1500, 800, 200, 200, "Chest" },
+                    { "Kevlar Vest", 800, 200, 400, 100, "Chest" },
+                    { "Robijnen Borstplaat", 2500, 1000, 200, 300, "Chest" },
+
+                    { "Lederen Handschoenen", 200, 100, 50, 150, "Hands" },
+                    { "Stalen Handschoenen", 500, 200, 100, 50, "Hands" },
+                    { "Magische Handschoenen", 1000, 300, 500, 200, "Hands" },
+
+                    { "Zegelring", 800, 200, 400, 100, "Ring" },
+                    { "Amethist Ring", 500, 100, 300, 50, "Ring" },
+                    { "Gouden Ring", 1200, 300, 200, 200, "Ring" },
+
+                    { "Diamanten Schoenen", 2000, 300, 200, 500, "Feet" },
+                    { "Nike Air Mag \"Back to the Future\"", 40000, 500, 500, 1000, "Feet" },
+                    { "Ninja schoenen", 500, -100, 200, 500, "Feet" },
+                    { "Hoge hakken", 300, 100, 400, -1000, "Feet" },
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryId",
+                table: "gear",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
